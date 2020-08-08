508d69c08e17608701a50b98f8373aa459d07641
ennas-de <dev.abdulhakeem@gmail.com>
2020-08-08 15:30:00 +0100

    Update 404

--- ui/pages/404.tsx
+++ ui/pages/404.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 
 const Custom404: NextPage = () => {
-  return <h1>404 - Page Not Found</h1>
+  return <h1>404 - Page Not Found.</h1>
 }
 
 export default Custom404
